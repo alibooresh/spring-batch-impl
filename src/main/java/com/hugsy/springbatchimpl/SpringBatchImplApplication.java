6364f9daed2d366b821876881ddd6070d9899e39
Ali Booresh <alibooresh1936@gmail.com>
2023-05-24 11:09:03 +0330

    Initial commit

---
+++ src/main/java/com/hugsy/springbatchimpl/SpringBatchImplApplication.java
@@ -0,0 +1,13 @@
+package com.hugsy.springbatchimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugsy/springbatchimpl/SpringBatchImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugsy.springbatchimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
